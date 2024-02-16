e41bed9c8e1f6aa84173c507e53642dbce23dc5d
Shivani Bandi <141583625+shivanibandi110@users.noreply.github.com>
2024-02-16 23:45:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,213 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(UnitConverterApp());
 }
 
-// With Flutter, you create user interfaces by combining "widgets"
-// You'll learn all about them (and much more) throughout this course!
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // Every custom widget must have a build() method
-  // It tells Flutter, which widgets make up your custom widget
-  // Again: You'll learn all about that throughout the course!
+class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Below, a bunch of built-in widgets are used (provided by Flutter)
-    // They will be explained in the next sections
-    // In this course, you will, of course, not just use them a lot but
-    // also learn about many other widgets!wx
     return MaterialApp(
-      title: 'Flutter First App',
-      theme: ThemeData(useMaterial3: true),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              Text(
-                'Flutter - The Complete Guide',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Learn Flutter step-by-step, from the ground up.',
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
+      title: 'Unit Converter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UnitConverterScreen(),
+    );
+  }
+}
+
+class UnitConverterScreen extends StatefulWidget {
+  @override
+  _UnitConverterScreenState createState() => _UnitConverterScreenState();
+}
+
+class _UnitConverterScreenState extends State<UnitConverterScreen> {
+  final List<String> units = ['meters', 'kilometers', 'miles', 'inches', 'feet'];
+  String selectedInputUnit = 'meters';
+  String selectedOutputUnit = 'kilometers';
+  double inputValue = 0;
+  double outputValue = 0;
+
+  void convert() {
+    setState(() {
+      switch (selectedInputUnit) {
+        case 'meters':
+          {
+            switch (selectedOutputUnit) {
+              case 'meters':
+                outputValue = inputValue;
+                break;
+              case 'kilometers':
+                outputValue = inputValue / 1000;
+                break;
+              case 'miles':
+                outputValue = inputValue * 0.000621371;
+                break;
+              case 'inches':
+                outputValue = inputValue * 39.3701;
+                break;
+              case 'feet':
+                outputValue = inputValue * 3.28084;
+                break;
+            }
+          }
+          break;
+        case 'kilometers':
+          {
+            switch (selectedOutputUnit) {
+              case 'meters':
+                outputValue = inputValue * 1000;
+                break;
+              case 'kilometers':
+                outputValue = inputValue;
+                break;
+              case 'miles':
+                outputValue = inputValue * 0.621371;
+                break;
+              case 'inches':
+                outputValue = inputValue * 39370.1;
+                break;
+              case 'feet':
+                outputValue = inputValue * 3280.84;
+                break;
+            }
+          }
+          break;
+        case 'miles':
+          {
+            switch (selectedOutputUnit) {
+              case 'meters':
+                outputValue = inputValue / 0.000621371;
+                break;
+              case 'kilometers':
+                outputValue = inputValue / 0.621371;
+                break;
+              case 'miles':
+                outputValue = inputValue;
+                break;
+              case 'inches':
+                outputValue = inputValue * 63360;
+                break;
+              case 'feet':
+                outputValue = inputValue * 5280;
+                break;
+            }
+          }
+          break;
+        case 'inches':
+          {
+            switch (selectedOutputUnit) {
+              case 'meters':
+                outputValue = inputValue * 0.0254;
+                break;
+              case 'kilometers':
+                outputValue = inputValue * 0.0000254;
+                break;
+              case 'miles':
+                outputValue = inputValue * 0.000015783;
+                break;
+              case 'inches':
+                outputValue = inputValue;
+                break;
+              case 'feet':
+                outputValue = inputValue * 0.0833333;
+                break;
+            }
+          }
+          break;
+        case 'feet':
+          {
+            switch (selectedOutputUnit) {
+              case 'meters':
+                outputValue = inputValue * 0.3048;
+                break;
+              case 'kilometers':
+                outputValue = inputValue * 0.0003048;
+                break;
+              case 'miles':
+                outputValue = inputValue * 0.000189394;
+                break;
+              case 'inches':
+                outputValue = inputValue * 12;
+                break;
+              case 'feet':
+                outputValue = inputValue;
+                break;
+            }
+          }
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Unit Converter'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Enter value to convert'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onChanged: (value) {
+                setState(() {
+                  inputValue = double.tryParse(value) ?? 0;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            DropdownButton<String>(
+              value: selectedInputUnit,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedInputUnit = newValue!;
+                });
+              },
+              items: units.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: convert,
+              child: Text('Convert'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Output:',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '$outputValue $selectedOutputUnit',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            DropdownButton<String>(
+              value: selectedOutputUnit,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedOutputUnit = newValue!;
+                });
+              },
+              items: units.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ],
         ),
       ),
     );
